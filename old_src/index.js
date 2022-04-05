522e32d4ab7dbdb23c1fa74c5b9ace563bd62254
Scott Park <scott.park.dev@gmail.com>
2022-04-05 16:02:44 -0700

    Idk why mobile version isn't responding well... Gonna build again from Moblie to Desktop

---
+++ old_src/index.js
@@ -0,0 +1,6 @@
+import './css/index.css';
+import { nav_display } from './pages/nav';
+import { welcome_page } from './pages/welcome';
+
+import { footer_section } from './pages/footer';
+
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import './css/index.css';
-import { nav_display } from './pages/nav';
-import { welcome_page } from './pages/welcome';
-
-import { footer_section } from './pages/footer';
-
