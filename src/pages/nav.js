eeaf1ab82152f4e42cb6f15128de676b94d8870c
Scott Park <scott.park.dev@gmail.com>
2022-04-06 15:01:23 -0700

    Layout for welcome page in mobile

--- src/pages/nav.js
+++ src/pages/nav.js
@@ -98,7 +98,7 @@ const navigation = (()=>{
         </span>
     </div>
     <div class="hamburger_menu">
-        <svg viewBox="0 0 100 80" width="30" height="30">
+        <svg viewBox="0 0 100 80" width="35" height="35">
             <rect width="100" height="20"></rect>
             <rect y="30" width="100" height="20"></rect>
             <rect y="60" width="100" height="20"></rect>
